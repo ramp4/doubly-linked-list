80704097a3dac997e4e3460f67f6398b40b1bc9e
ramp4 <rampatv13@gmail.com>
2019-10-11 23:38:51 +0300

    feat: implement some methods of LinkedList

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,100 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this.length = 0;
+    this._head = null;
+    this._tail = null;
+  }
 
-    append(data) {}
+  append(data) {
+    if (this.isEmpty()) {
+      let node = new Node(data);
+      this._head = node;
+      this._tail = node;
+      this.length++;
+      return this;
+    }
+    let node = new Node(data, this._tail);
+    this.length++;
+    this._tail = node;
+    this._tail.prev.next = node;
+    return this;
+  }
 
-    head() {}
+  head() {
+    return this._head.data;
+  }
 
-    tail() {}
+  tail() {
+    return this._tail.data;
+  }
 
-    at(index) {}
+  at(index) {
+    let node = this._head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+    return node.data;
+  }
 
-    insertAt(index, data) {}
+  insertAt(index, data) {
+    let insertNode = new Node(data);
+    let node = this._head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+    let prevNode = node.prev;
+    insertNode.prev = prevNode;
+    insertNode.next = node;
+    prevNode.next = insertNode;
+    node.prev = insertNode;
+    this.length++;
+  }
 
-    isEmpty() {}
+  isEmpty() {
+    return !this.length;
+  }
 
-    clear() {}
+  clear() {
+    this.length = 0;
+    this._tail.data = null;
+    this._head.data = null;
+  }
 
-    deleteAt(index) {}
+  deleteAt(index) {
+    let node = this._head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+    let prevNode = node.prev;
+    let nextNode = node.next;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+    this.length--;
+  }
 
-    reverse() {}
+  reverse() {
+    let length = this.length;
+    let storage = [];
+    for (let i = 0; i < length; i++) {
+      let data = this.at(i);
+      storage.push(data);
+      storage.reverse();
+    }
+    this.clear();
+    for (let i = 0; i < length; i++) {
+      this.append(storage[i]);
+    }
+    return this;
+  }
 
-    indexOf(data) {}
+  indexOf(data) {
+    for (let i = 0; i < this.length; i++) {
+      if (this.at(i) == data) return i;
+    }
+    return -1;
+  }
 }
 
 module.exports = LinkedList;
